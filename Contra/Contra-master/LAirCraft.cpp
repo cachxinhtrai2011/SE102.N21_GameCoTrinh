1a0705105e6f014386671895a1849fdc40b3028f
MSI\MIT <ltt.9a1.dtthuan@gmail.com>
2023-05-31 23:32:42 +0700

    Up LaiCraft

---
+++ Contra/Contra-master/LAirCraft.cpp
@@ -0,0 +1,1 @@
+#include "LAirCraft.h"
---
+++ Contra/Contra-master/LAirCraft.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "AirCraft.h"
+#define ID_ANI_LARICRAFT 1100
+class CLAirCraft: public CAirCraft
+{
+public:
+	CLAirCraft(float x, float y) :CAirCraft(x, y)
+	{
+		LPSPRITE sprite = CSprites::GetInstance()->Get(ID_ANI_LARICRAFT);
+		if (sprite == NULL)
+			this->LoadAnimation();
+	}
+	void Render()
+	{
+		CAnimations::GetInstance()->Get(ID_ANI_LARICRAFT)->Render(x, y);
+	}
+	static void LoadAnimation()
+	{
+		LPTEXTURE texture = CTextures::GetInstance()->Get(AIRCRAFT_TEXTURE_ID);
+		if (texture == NULL)
+		{
+			CAirCraft::LoadAnimation();
+			texture = CTextures::GetInstance()->Get(AIRCRAFT_TEXTURE_ID);
+		}
+		CSprites* sprites = CSprites::GetInstance();
+		sprites->Add(ID_ANI_LARICRAFT, 80, 0, 105, 15, texture);
+		LPANIMATION ani = new CAnimation(100);
+		ani->Add(ID_ANI_LARICRAFT);
+		CAnimations::GetInstance()->Add(ID_ANI_LARICRAFT, ani);
+	}
+};
+
